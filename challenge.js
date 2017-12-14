78a233cd40c248f319ded4cc416af6e2339c069f
c0nniewang <connie.wang303@gmail.com>
2017-12-14 16:44:37 -0500

    doneish

--- challenge.js
+++ challenge.js
@@ -1,10 +1,15 @@
 document.addEventListener("load", setInterval(incrementTime, 1000));
 
 let counterNumber = document.getElementById("counter")
-let seconds = 0;
+let seconds = 0
+let pause = false
 
 function incrementTime() {
-  counterNumber.innerText = seconds++
+  if (pause === true){
+    seconds
+  } else {
+    counterNumber.innerText = seconds++
+  }
 }
 
 document.getElementById("+").addEventListener("click", increment);
@@ -33,11 +38,38 @@ function addLikes() {
   let likedBullets = document.createElement('li');
 
   likedBullets.innerHTML = `${seconds} has been liked ${likedNumbers[seconds]} time(s).`
-  console.log(likedBullets)
 
   document.querySelector(".likes").appendChild(likedBullets)
 }
 
+document.getElementById("pause").addEventListener("click", pauseGame)
+
+function pauseGame() {
+  pause = !pause
+  if (pause === true){
+    document.getElementById("+").disabled = true;
+    document.getElementById("-").disabled = true;
+    document.getElementById("<3").disabled = true;
+    document.getElementById("submit").disabled = true;
+    document.getElementById("pause").innerText = "resume"
+  } else {
+    document.getElementById("+").disabled = false;
+    document.getElementById("-").disabled = false;
+    document.getElementById("<3").disabled = false;
+    document.getElementById("submit").disabled = false;
+    document.getElementById("pause").innerText = "pause"
+  }
+}
+
+document.getElementById("submit").addEventListener("click", addComment)
+
+function addComment(event) {
+  event.preventDefault()
+  let comment = document.getElementsByTagName("input")[0].value
+  let commentPost = document.createElement('p')
+  commentPost.innerText = comment
+   document.getElementById("list").appendChild(commentPost)
+}
 
 
 
